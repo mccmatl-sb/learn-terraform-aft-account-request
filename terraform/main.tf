8fe364ada262137c844881f3a6ff88bb63d98acd
Matt Humbert <matthumbert@Matts-MacBook-Air.local>
2024-07-21 18:04:00 -0500

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "matt@biasedvariance.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "AFT Management"
+    SSOUserEmail              = "matt@biasedvariance.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
